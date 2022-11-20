56702df28a08be104c2b49ee529fea78e1f8e67f
gushiii <1726143054@qq.com>
2022-11-20 14:03:15 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+// 全局注入 ElementUI
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
